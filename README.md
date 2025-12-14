b6b94e5df72e4edb3243ed5623673e9acf6b5ced
ericGutierrezM <gh.ericgutierrez@gmail.com>
2025-12-14 16:47:59 +0100

    add structure

---
+++ README.md
@@ -0,0 +1,11 @@
+# Predicting First Downs in the NFL
+#### Computing for Data Science | Barcelona School of Economics | December 2025
+> Daniel Campos & Eric Gutiérrez
+---
+---
+
+## About the dataset: ```nflreadpy```
+
+
+## uv and required libraries
+- Pandas
---
+++ src/firstDown/preprocessing/deal_nan.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def search_nan(): # return a table with the columns with nan, number of nans, ideally cross-distribution of nan
+    return True
+
+def replace_nan(): # with an argument on how to deal with them
+    return True
+
+def drop_nan():
+    return True
